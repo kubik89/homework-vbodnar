7225cdfe6d9569b83ecf43d56755e32d05d0e9f4
Volodymyr <kubik89@yahoo.com>
2020-07-10 21:12:15 +0300

    first part Only

--- src/owu-homework/dz-les4-Functions.js
+++ src/owu-homework/dz-les4-Functions.js
@@ -1,4 +1,4 @@
-let bArr = [];
+let bArr = [1,2,8];
 let a = 10;
 let b = 7;
 let c = 11;
@@ -7,6 +7,8 @@ let c = 11;
 function createNewArr () {
     return [bArr];
 }
+
+console.log(createNewArr());
 
 // створити функцію яка заповнює масив рандомними числами та виводить його. Для виведення використати
 // попвередню функцію.
@@ -17,13 +19,35 @@ for (let i = 0; i < 10; i++) {
     return createNewArr();
 }
 
+console.log(addRandomToCreateNewArrFunc());
+
 // - створити функцію яка приймає три числа та виводить та повертає найменьше.
-function smallerElement() {
-if (a<b && a<c) {
-    return a;
+function SmallerElement(a1=5,b1=2,c1=3) {
+if (a1<b1 && a1<c1) {
+    return a1;
 }
-else if (b<a && b<c) {
-    return b;
+else if (b1<a1 && b1<c1) {
+    return b1;
 }
-else return c;
+else return c1;
 }
+
+
+// - створити функцію яка приймає три числа та виводить та повертає найбільше.
+
+function BiggerElement(myA, myB, myC) {
+    if (myA > myB && myA > myC) {
+        return myA;
+    }
+    else if (myB>myA && myB>myC) {
+        return myB;
+    }
+    else
+        return myC;
+    return myA+myB+myC;
+}
+
+let biggerElement = BiggerElement(14, 10, 7);
+console.log(biggerElement);
+
+// - створити функцію яка приймає будь-яку кількість чисел, повертає найменьше, а виводить найбільше
