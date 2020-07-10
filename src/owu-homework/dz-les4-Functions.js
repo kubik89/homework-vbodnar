5bb11b97063564dfcf97d21ebe6c6fe40948ff77
Volodymyr <kubik89@yahoo.com>
2020-07-10 17:24:29 +0300

    first part Only

--- src/owu-homework/dz-les4-Functions.js
+++ src/owu-homework/dz-les4-Functions.js
@@ -1,4 +1,7 @@
 let bArr = [];
+let a = 10;
+let b = 7;
+let c = 11;
 
 // створити функцію яка виводить масив
 function createNewArr () {
@@ -12,4 +15,15 @@ for (let i = 0; i < 10; i++) {
     bArr.push(Math.floor(Math.random() * 10));
     }
     return createNewArr();
+}
+
+// - створити функцію яка приймає три числа та виводить та повертає найменьше.
+function smallerElement() {
+if (a<b && a<c) {
+    return a;
+}
+else if (b<a && b<c) {
+    return b;
+}
+else return c;
 }
