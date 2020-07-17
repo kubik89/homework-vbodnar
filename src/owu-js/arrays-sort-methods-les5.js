695e706b0daeb0670a3f0044c15d8958bf6f957f
Volodymyr <kubik89@yahoo.com>
2020-07-17 13:34:18 +0300

    done - при помощи filter получить числа кратные 10

--- src/owu-js/arrays-sort-methods-les5.js
+++ src/owu-js/arrays-sort-methods-les5.js
@@ -1,22 +1,28 @@
 let numb = [1,99,-800,74,550,-17,65];
 let str = ["Vova", "Vasya", "Vasylyna", "Ira", "Andrew"];
-numb.sort((a, b) => {
-    return a - b;
-});
+// numb.sort((a, b) => {
+//     return a - b;
+// });
 // console.log(numb);
 
-str.sort((a, b) => {
-    if (a < b) {
-        return -1
-    } return 1
-})
+// str.sort((a, b) => {
+//     if (a < b) {
+//         return -1
+//     } return 1
+// });
 // console.log(str);
-let user = [
-    {name: "Vova", age: 30},
-    {name: "Max", age: 40},
-    {name: "Ira", age: 15}
-];
-user.sort((a, b) => {
-    return a.age - b.age;
-})
-console.log(user);
+// let user = [
+//     {name: "Vova", age: 30},
+//     {name: "Max", age: 40},
+//     {name: "Ira", age: 15}
+// ];
+// user.sort((a, b) => {
+//     return a.age - b.age;
+// });
+// console.log(user);
+
+// let filtered = numb.filter((value, index) => {
+//    return value%2 === 0;
+// });
+// console.log(filtered);
+
