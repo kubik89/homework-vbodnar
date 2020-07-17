dcdfb3accd86af3c6fa0d705d7d123458f3af0d3
Volodymyr <kubik89@yahoo.com>
2020-07-17 12:43:06 +0300

    sort

---
+++ src/owu-js/arrays-sort-methods-les5.js
@@ -0,0 +1,22 @@
+let numb = [1,99,-800,74,550,-17,65];
+let str = ["Vova", "Vasya", "Vasylyna", "Ira", "Andrew"];
+numb.sort((a, b) => {
+    return a - b;
+});
+// console.log(numb);
+
+str.sort((a, b) => {
+    if (a < b) {
+        return -1
+    } return 1
+})
+// console.log(str);
+let user = [
+    {name: "Vova", age: 30},
+    {name: "Max", age: 40},
+    {name: "Ira", age: 15}
+];
+user.sort((a, b) => {
+    return a.age - b.age;
+})
+console.log(user);
