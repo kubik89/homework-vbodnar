89956a455bf2837e66b72297a9dad8b2aeb362de
Volodymyr <kubik89@yahoo.com>
2020-07-30 18:08:43 +0300

    async await 15%

--- src/owu-homework/dz-eventLoopAsync.js
+++ src/owu-homework/dz-eventLoopAsync.js
@@ -13,64 +13,226 @@
 //     Якщо щось пішло не так (нема шо їсти), то має бути викинута помилка і решта функцій виконуватись не мають.
 //     Якщо ж все ок, то ви маєте прожити свій звичайний день.
 //     Кожна подія має бути з рандомною (не по зростанню) затримкою.
-getUp(true, (err, result) => {
-   if (err) {
-       console.error(err, " Почуваюсь погано. ");
-   } else {
-       console.log("Спав добре. ", result + " калорій");
 
-       eatBreakfast(false, (err, rest) => {
-            if (err) {
-                console.log(err, "Немає що їсти. День пропав. НІчого не роблю далі. ")
-            } else {
-                console.log("Поїв та набрав разом ", rest)
-            }
+                //Через колбеки
 
-            cleaning(true, (err, rest) => {
-                if (err) {
-                    console.error("Без води немає життя, нічого не роблю!")
-                } else {
-                   console.log("Після миття залишилось " + rest + " калорій. ")
-                }
-            })
-        });
-   }
-});
+// getUp(true, (err, result) => {
+//    if (err) {
+//        console.error(err, " Почуваюсь погано. ");
+//    } else {
+//        console.log("Спав добре. ", result + " калорій");
+//
+//        eatBreakfast(false, (err, rest) => {
+//             if (err) {
+//                 console.error(err, "Немає що їсти. День пропав. НІчого не роблю далі. ")
+//             } else {
+//                 console.log("Поїв та набрав разом ", rest);
+//
+//                 cleaning(false, (err, rest) => {
+//                     if (err) {
+//                         console.error("Без води немає життя, нічого не роблю!")
+//                     } else {
+//                        console.log("Після миття залишилось " + rest + " калорій. ");
+//
+//                         lectionOverview(true, (err, rest) => {
+//                             if (err) {
+//                                 console.error("Помилка огляду відео.", err)
+//                             } else {
+//                                 console.log("Залишок калорій - " + rest);
+//
+//                                 haveLunch(true, (err, restAftLunch) => {
+//                                     if (err) {
+//                                         console.error("Немає що обідати")
+//                                     } else {
+//                                         console.log(restAftLunch)
+//                                     }
+//                                 });
+//                             }
+//                         });
+//                     }
+//                 })
+//             }
+//        });
+//    }
+// });
+//
+// let callor = 0;
+// function getUp(isOpenEyes, cb) { // якщо пркинувся й біль в животі
+//     setTimeout(() => {
+//         console.log("I can eat and have " + callor + " callories");
+//         if (isOpenEyes) {
+//             callor+=500;
+//             console.log("I have " + callor + " callories");
+//             cb(null, callor)
+//         } else {
+//             cb("Ранок почався так собі. ", null)
+//         }
+//     }, 500);
+// }
+//
+// function eatBreakfast(noFood, cb) {
+//     setTimeout(() => {
+//         if (noFood) {
+//             cb("Need to go shop. ", null);
+//         } else {
+//             callor += 1000;
+//             cb(null, callor + " калорій. ") // rest - callor + " калорій. "
+//         }
+//     }, 100)
+// }
+//
+// function cleaning(noWater, cb) {
+//     setTimeout(() => {
+//         if (noWater) {
+//             cb("No water for cleaning. ", null);
+//         } else {
+//             callor-=200;
+//             cb(null, callor)
+//         }
+//     }, 400)
+// }
+//
+// function lectionOverview(isVideoPresent, cb) {
+//    setTimeout(() => {
+//        if(isVideoPresent) {
+//            console.log("Вивчаю лекцію... ");
+//            cb(null, callor-=450);
+//            return lectionFinish = true
+//        } else {
+//            cb("Віктор не залив відео, день пропав", null);
+//        }
+//    },700)
+// }
+//
+// function haveLunch(isHaveFood, cb) {
+//     setTimeout( () => {
+//         if (isHaveFood) {
+//             callor+=1000;
+//             cb(null, "Після обіду залишок калорій " +  callor)
+//         } else {
+//             cb("No food for lunch", null)
+//         }
+//     }, 10)
+// }
+
+                //Через проміси
+
+// getUp(true).then(morningCallories => { // при that - це те що приходить із Reject, тобто успішне
+//     console.log(morningCallories);
+//
+//     return eatBreakfast (false, morningCallories)
+// }).then(afterBreakfCallor => {
+//     console.log(afterBreakfCallor);
+//
+//     return cleaning(false, afterBreakfCallor)
+// }).then(afterCleaningCallor => {
+//     console.log(afterCleaningCallor);
+//
+//     return lectionOverview (true, afterCleaningCallor)
+// }).then(afterLectionCallor => {
+//     console.log(afterLectionCallor);
+//
+//     return haveLunch(true, afterLectionCallor)
+// }).then(afterLunchCallor => {
+//     console.log(afterLunchCallor);
+// }).catch(reason => {
+//     console.error(reason)
+// });
+//
+// let callor = 0;
+// function getUp(isOpenEyes) { // якщо пркинувся й біль в животі
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             console.log("I can eat and have " + callor + " callories");
+//             if (isOpenEyes) {
+//                 callor+=500;
+//                 console.log("I have " + callor + " callories");
+//                 resolve(callor)
+//             } else {
+//                 reject("Ранок почався так собі. ")
+//             }
+//         }, 500);
+//     })
+// }
+//
+// function eatBreakfast(noFood) {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             if (noFood) {
+//                 reject("Need to go shop. ");
+//             } else {
+//                 callor += 1000;
+//                 resolve(callor + " калорій. ")
+//             }
+//         }, 100)
+//     })
+// }
+//
+// function cleaning(noWater) {
+//     return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         if (noWater) {
+//             reject ("No water for cleaning. ");
+//         } else {
+//             callor-=200;
+//             resolve("Після миття залишилось " + callor)
+//         }
+//     }, 400)
+//     })
+// }
+//
+// function lectionOverview(isVideoPresent) {
+//     return new Promise((resolve, reject) => {
+//    setTimeout(() => {
+//        if(isVideoPresent) {
+//            console.log("Вивчаю лекцію... ");
+//            callor-=450;
+//            resolve( "Калорій після огляду лекції " + callor);
+//        } else {
+//            reject("Віктор не залив відео, день пропав");
+//        }
+//    },700)
+//     })
+// }
+//
+// function haveLunch(isHaveFood) {
+//     return new Promise((resolve, reject) => {
+//     setTimeout( () => {
+//         if (isHaveFood) {
+//             callor+=1000;
+//             resolve("Після обіду залишок калорій " +  callor)
+//         } else {
+//             reject("No food for lunch")
+//         }
+//     }, 10)
+//     })
+// }
+
+
+                // Через асинк авейт.
+
+async function myDailyJobs() {
+    try {
+        await getUp(true);
+        console.log(callor)
+
+    } catch (e) {
+
+    }
+}
 
 let callor = 0;
-function getUp(isOpenEyes, cb) { // якщо пркинувся й біль в животі
-    setTimeout(() => {
-        console.log("I can eat and have " + callor + " callories");
-        if (isOpenEyes) {
-            callor+=500;
-            console.log("I have " + callor + " callories");
-            cb(null, callor)
-        } else {
-            cb("Ранок почався так собі. ", null)
-        }
-    }, 500);
+function getUp(isOpenEyes) { // якщо пркинувся й біль в животі
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log("I can eat and have " + callor + " callories");
+            if (isOpenEyes) {
+                callor+=500;
+                console.log("I have " + callor + " callories");
+                resolve(callor)
+            } else {
+                reject("Ранок почався так собі. ")
+            }
+        }, 500);
+    })
 }
-
-function eatBreakfast(noFood, cb) {
-    setTimeout(() => {
-        if (noFood) {
-            cb("Need to go shop. ", null);
-        } else {
-            callor += 1000;
-            cb(null, callor + " калорій. ") // rest - callor + " калорій. "
-        }
-    }, 600)
-}
-
-function cleaning(noWater, cb) {
-    setTimeout(() => {
-        if (noWater) {
-            cb("No water for cleaning. ", null);
-        } else {
-            callor-=200;
-            cb(null, callor)
-        }
-    }, 500)
-}
-
-
--- src/owu-js/eventLoopAsync.js
+++ src/owu-js/eventLoopAsync.js
@@ -66,92 +66,92 @@
 // });
 
 // Колбек в колбеці
-function clearHouse(isJobDone, callB) {
-    setTimeout(() => {
-        if (isJobDone) {
-            console.log("I'm cleaning yet");
-        callB(null, 100); // спочатку передаю помилку, а потім дані
-        } else {
-            callB("Грошей немає, нічого не заробив", null) // в else навпаки - спочатку дані, а потім помилку
-        }
-    }, 100)
-}
-
-function buyClothes(money, cb) {
-    setTimeout(() => {
-        if (money > 200) {
-            console.log("It's enough to buy good jeans");
-            money-=200; // Тобто потратив 200грн на покупку джинсів
-
-            cb(null, money)
-
-        } else {
-            cb("Errrror, no enough money... ", null) // одяг не купив, тому null як дані - нічого.
-        }
-    }, 1000)
-}
-clearHouse(true, (err, reward) => { // ПРАВИЛО "Error first, data last"
-    if (err) {
-        console.error(err)
-    } else {
-    console.log("________");
-    console.log(err);
-    console.log(reward);
-    console.log("________");
-    buyClothes(reward, (err, rest) => {  // запускаю тут свою функцію buyClothes, бо вона залажна від clearHouse, оскільки
-                                    // якщо немає грошей (reward) за прибирання, то немає за що купити одяг
-        if (err) {
-            console.log(err)
-        } else {
-            console.log("Після покупки одягу залишилось " + rest)
-        }
-    })
-    }
-});
-
-
-// PROMISE
-
-// function clearHouse(isJobDone) { // в Promise не потрібно callback-ів
-//     return new Promise((resolve, reject) => {   // створюю новий Promise, тут resolve - це успішне виконання,
-//                                                         // а reject - не успішне виконання
+// function clearHouse(isJobDone, callB) {
 //     setTimeout(() => {
 //         if (isJobDone) {
 //             console.log("I'm cleaning yet");
-//             resolve(550); // resolve - це функція як при УСПІХУ, яка передає ЛИШЕ ОДИН параметр
+//         callB(null, 100); // спочатку передаю помилку, а потім дані
 //         } else {
-//             reject("Грошей немає, нічого не заробив") // reject - як щось пішло не так. передаємо причину поломки
+//             callB("Грошей немає, нічого не заробив", null) // в else навпаки - спочатку дані, а потім помилку
 //         }
 //     }, 100)
-//     })
 // }
 //
-// function buyClothes(money) { // в Promise не потрібно callback-ів
-//     return new Promise((resolve, reject) => {
+// function buyClothes(money, cb) {
 //     setTimeout(() => {
 //         if (money > 200) {
 //             console.log("It's enough to buy good jeans");
 //             money-=200; // Тобто потратив 200грн на покупку джинсів
 //
-//             resolve (money);
+//             cb(null, money)
 //
 //         } else {
-//             reject ("Errrror, no enough money... ") //
+//             cb("Errrror, no enough money... ", null) // одяг не купив, тому null як дані - нічого.
 //         }
 //     }, 1000)
-//     })
 // }
-// // Promise chain - коли замість цепочки then-ів повертаєш результат функції, що раніше виконалась
-// clearHouse(true).
-//     then(resolve => { // якщо мій resolve у clearHouse виконався, то показати його value (можна писати value або resolve)
-//     console.log(resolve);
-//     return buyClothes(resolve);
-//
-// }).then(rest => {
-//     console.log("___________");
-//     console.log("Після покупки одягу залишилось " + rest);  // оскільки я повернув buyClothes(resolve), то тут у якусь
-//                                                             // змінну rest я отримав його результат
+// clearHouse(true, (err, reward) => { // ПРАВИЛО "Error first, data last"
+//     if (err) {
+//         console.error(err)
+//     } else {
+//     console.log("________");
+//     console.log(err);
+//     console.log(reward);
+//     console.log("________");
+//     buyClothes(reward, (err, rest) => {  // запускаю тут свою функцію buyClothes, бо вона залажна від clearHouse, оскільки
+//                                     // якщо немає грошей (reward) за прибирання, то немає за що купити одяг
+//         if (err) {
+//             console.log(err)
+//         } else {
+//             console.log("Після покупки одягу залишилось " + rest)
+//         }
+//     })
+//     }
 // });
+
+
+// PROMISE
+
+function clearHouse(isJobDone) { // в Promise не потрібно callback-ів
+    return new Promise((resolve, reject) => {   // створюю новий Promise, тут resolve - це успішне виконання,
+                                                        // а reject - не успішне виконання
+    setTimeout(() => {
+        if (isJobDone) {
+            console.log("I'm cleaning yet");
+            resolve(550); // resolve - це функція як при УСПІХУ, яка передає ЛИШЕ ОДИН параметр
+        } else {
+            reject("Грошей немає, нічого не заробив") // reject - як щось пішло не так. передаємо причину поломки
+        }
+    }, 100)
+    })
+}
+
+function buyClothes(money) { // в Promise не потрібно callback-ів
+    return new Promise((resolve, reject) => {
+    setTimeout(() => {
+        if (money > 200) {
+            console.log("It's enough to buy good jeans");
+            money-=200; // Тобто потратив 200грн на покупку джинсів
+
+            resolve (money);
+
+        } else {
+            reject ("Errrror, no enough money... ") //
+        }
+    }, 1000)
+    })
+}
+// Promise chain - коли замість цепочки then-ів повертаєш результат функції, що раніше виконалась
+clearHouse(true).
+    then(resolve => { // якщо мій resolve у clearHouse виконався, то показати його value (можна писати value або resolve)
+    console.log(resolve);
+    return buyClothes(resolve);
+
+}).then(rest => {
+    console.log("___________");
+    console.log("Після покупки одягу залишилось " + rest);  // оскільки я повернув buyClothes(resolve), то тут у якусь
+                                                            // змінну rest я отримав його результат
+});
 
 // Хоча можна й писати як внизу, але цими цепочками важче читати
 // clearHouse(true).
@@ -248,36 +248,36 @@ clearHouse(true, (err, reward) => { // ПРАВИЛО "Error first, data last"
 //     console.log(value);
 // });
 
-function clearHouse(isJobDone) { // в Promise не потрібно callback-ів
-    return new Promise((resolve, reject) => {   // створюю новий Promise, тут resolve - це успішне виконання,
-        // а reject - не успішне виконання
-        setTimeout(() => {
-            if (isJobDone) {
-                console.log("I'm cleaning yet");
-                resolve(550); // resolve - це функція як при УСПІХУ, яка передає ЛИШЕ ОДИН параметр
-            } else {
-                reject("Грошей немає, нічого не заробив") // reject - як щось пішло не так. передаємо причину поломки
-            }
-        }, 100)
-    })
-}
-
-function buyClothes(money) { // в Promise не потрібно callback-ів
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            if (money > 200) {
-                console.log("It's enough to buy good jeans");
-                money-=200; // Тобто потратив 200грн на покупку джинсів
-
-                resolve (money);
-
-            } else {
-                reject ("Errrror, no enough money... ") //
-            }
-        }, 1000)
-    })
-}
-
+// function clearHouse(isJobDone) { // в Promise не потрібно callback-ів
+//     return new Promise((resolve, reject) => {   // створюю новий Promise, тут resolve - це успішне виконання,
+//         // а reject - не успішне виконання
+//         setTimeout(() => {
+//             if (isJobDone) {
+//                 console.log("I'm cleaning yet");
+//                 resolve(550); // resolve - це функція як при УСПІХУ, яка передає ЛИШЕ ОДИН параметр
+//             } else {
+//                 reject("Грошей немає, нічого не заробив") // reject - як щось пішло не так. передаємо причину поломки
+//             }
+//         }, 100)
+//     })
+// }
+//
+// function buyClothes(money) { // в Promise не потрібно callback-ів
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             if (money > 200) {
+//                 console.log("It's enough to buy good jeans");
+//                 money-=200; // Тобто потратив 200грн на покупку джинсів
+//
+//                 resolve (money);
+//
+//             } else {
+//                 reject ("Errrror, no enough money... ") //
+//             }
+//         }, 1000)
+//     })
+// }
+//
 async function houseCleaner() { // async - Означає що в середині функції буде виконуватися асинхронна робота
     try {
         let reward = await clearHouse(true); // почекає поки виконається функція clearHouse, без цього не
