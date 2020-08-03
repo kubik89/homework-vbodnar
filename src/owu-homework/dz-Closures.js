b3fd7c8bf010de4ea0c9bd5832d15ce73a5e2522
Volodymyr <kubik89@yahoo.com>
2020-08-03 17:50:38 +0300

    working with Unique numbers of Card

--- src/owu-homework/dz-Closures.js
+++ src/owu-homework/dz-Closures.js
@@ -1,50 +1,82 @@
-function getCardOptions() {
-    let balance = 100;
-    let transactionLimit = 100;
-    let historyLogs = [];
-    let key = 1;
+function userCard(number) {
+    let card1 = {
+        cardOpt:
+            function getCardOptions() {
+                let cardNumber = number;
+                let balance = 100;
+                let transactionLimit = 100;
+                let historyLogs = [];
+                let key = setUniqueCardNumber();
 
-    function getBalance() {
-        console.log(balance);
-    }
-
-    function getTransactionLimit() {
-        console.log(transactionLimit);
-    }
-    function setTransactionLimit(money) {
-        if (money > 100) {
-            console.error("Перевищений максимум")
-        } else {
-            transactionLimit = money;
-        }
-    }
-    function putCredits(money) {
-        balance += money;
-        console.log(balance);
-    }
-    function takeCredits(money) {
-        if (balance > money) {
-            console.log("Баланс більший за гроші");
-                if (money < transactionLimit) {
-                balance -= money;
-                console.log("Успішно. На залишку тепер " + balance);
-            } else {
-                    console.error("Помилка ліміту")
+                function getBalance() {
+                    console.log(balance);
                 }
-        }
-        else {
-            console.error("Помилка балансу");
-        }
-    }
-    return {
-        getBalance,
-        putCredits,
-        getTransactionLimit,
-        setTransactionLimit,
-        takeCredits
-    }
+
+                function getCardNumber() {
+                    return cardNumber;
+                }
+
+                function setUniqueCardNumber() {
+                    let x = Math.random() * 1000;
+                    let uniqueValue = new Set;
+                    uniqueValue.add(key);
+                    cardNumber = Math.floor(x);
+                    return Math.floor(x);
+                }
+
+                function getKey() {
+                    console.log(key)
+                }
+
+                function getTransactionLimit() {
+                    console.log(transactionLimit);
+                }
+
+                function setTransactionLimit(money) {
+                    if (money > 100) {
+                        console.error("Перевищений максимум")
+                    } else {
+                        transactionLimit = money;
+                    }
+                }
+
+                function putCredits(money) {
+                    balance += money;
+                    console.log(balance);
+                }
+
+                function takeCredits(money) {
+                    if (balance > money) {
+                        console.log("Баланс більший за гроші");
+                        if (money < transactionLimit) {
+                            balance -= money;
+                            console.log("Успішно. На залишку тепер " + balance);
+                        } else {
+                            console.error("Помилка ліміту")
+                        }
+                    } else {
+                        console.error("Помилка балансу");
+                    }
+                }
+                // function transferCredits(creditsQuantity, card1) {
+                //
+                // }
+
+                return {
+                    getBalance,
+                    putCredits,
+                    getTransactionLimit,
+                    setTransactionLimit,
+                    takeCredits,
+                    getCardNumber,
+                    setUniqueCardNumber,
+                    getKey
+                }
+            }
+    };
+    return card1
 }
-let aboutCard = getCardOptions();
-aboutCard.getBalance();
-aboutCard.putCredits(200);
-aboutCard.takeCredits(110);
+
+let card3 = userCard(1).cardOpt();
+console.log(card3.getCardNumber());
+// card3.getKey();
