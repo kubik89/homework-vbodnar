2c2d8bafabacf70ed1a215712665412b703b5b17
Volodymyr <kubik89@yahoo.com>
2020-08-06 18:02:51 +0300

    100% done

--- src/owu-homework/dz-Closures.js
+++ src/owu-homework/dz-Closures.js
@@ -12,36 +12,65 @@ function userCard(number) {
     }
 
     function putCredits(money) {
-        // return cardOptions.balance+= money;
         cardOptions.balance += money;
+        cardOptions.historyLogs.push({
+            OperationType: `Received Credits`,
+            credits: `${money}`,
+            operationTime: `${time}, ${date}`
+        })
     }
 
     function setTransactionLimit(money) {
-            cardOptions.transactionLimit = money;
-        }
+        cardOptions.transactionLimit = money;
+        cardOptions.historyLogs.push({
+            OperationType: `transaction Limit Change`,
+            credits: `${money}`,
+            operationTime: `${time}, ${date}`
+        })
+    }
+
+    function getTransactionLimit() {
+        return cardOptions.transactionLimit;
+    }
 
     function takeCredits(money) {
-        if (cardOptions.balance > money) {
-            console.log("Баланс більший за гроші");
-            if (money < cardOptions.transactionLimit) {
-                cardOptions.balance -= money;
-                console.log("Успішно. На залишку тепер " + cardOptions.balance);
-            } else {
-                console.error("Недостатньо коштів на рахунку")
-            }
+        if (cardOptions.balance > money && money < cardOptions.transactionLimit) {
+            cardOptions.balance -= money;
+
+            cardOptions.historyLogs.push({
+                OperationType: `Withdrawal of credits`,
+                credits: `${money}`,
+                operationTime: `${time}, ${date}`
+            });
+            return money;
         } else {
-            console.error("Баланс менший за гроші");
+            console.error("Недостатньо коштів на рахунку або Баланс менший за гроші");
         }
     }
+
+    function transferCredits(money, card) {
+        takeCredits(money);
+        card.putCredits(money)
+    }
+
+    // cardOptions.historyLogs.forEach(value => {
+    //     if (value>0) {
+    //         console.log("Більше нуля")
+    //     } else console.log("Менше нуля")
+    // })
 
     return {
         getCardOptions,
         putCredits,
         setTransactionLimit,
+        getTransactionLimit,
         takeCredits,
+        transferCredits
     }
 }
 
+let date = new Date().toLocaleDateString();
+let time = new Date().toLocaleTimeString();
 
 class UserAccount {
 
@@ -51,7 +80,7 @@ class UserAccount {
     }
 
     getCardByKey(cardNumber) {
-        if (cardNumber > 0 && cardNumber <= 3){
+        if (cardNumber > 0 && cardNumber <= 3) {
             return userCard(cardNumber)
         } else console.log("Така кількість карт не можлива")
     }
@@ -72,40 +101,17 @@ let vova = new UserAccount("Volodymyr");
 vova.addCard("Mono");
 vova.addCard("Privat");
 vova.addCard("GasBank");
+
+
+let card1 = vova.getCardByKey(1);
 let card2 = vova.getCardByKey(2);
+
+card1.putCredits(500);
+card1.setTransactionLimit(800);
+card1.transferCredits(300, card2);
+console.log(card1.getCardOptions());
+
+console.log(card2.getCardOptions());
+card2.takeCredits(50);
 console.log(card2.getCardOptions());
 
-card2.putCredits(280);
-console.log(card2.getCardOptions());
-console.log(card2.takeCredits(80));
-card2.setTransactionLimit(500);
-console.log(card2.getCardOptions());
-
-
-// console.log(vova.cards);
-// console.log(userCard(2).getCardOptions());
-// userCard(2).putCredits(200);
-// console.log(userCard(2).getCardOptions());
-
-
-// for (let i = 0; i < this.cards.length; i++) {
-    // if (this.cards.length === 0 ) {
-    //     this.cards.push(1);
-    //     console.log("Нічого не пушнув - 0");
-    //     return;
-//     if ((this.cards[i] === 1 || this.cards[i] === 2) && (this.cards[i + 1] !== 3)) {
-//         this.cards.push(3);
-//         console.log("пушнув - 3");
-//         return;
-//     } else if ((this.cards[i] === 2 || this.cards[i] === 3) && (this.cards[i + 1] !== 1)) {
-//         this.cards.push(1);
-//         console.log("пушнув - 1");
-//         return;
-//     } else if ((this.cards[i] === 1 || this.cards[i] === 3) && (this.cards[i + 1] !== 2)) {
-//         this.cards.push(2);
-//         console.log("пушнув - 2");
-//         return;
-//     } else if (this.cards[0] === undefined ) {
-//         this.cards.push(1);
-//     } else console.log("Нічого не пушнув")
-// }
