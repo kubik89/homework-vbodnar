c9a7d2dc16f9f51b1a7a20409aedf650405648ba
Volodymyr <kubik89@yahoo.com>
2020-08-19 13:21:07 +0300

    vue - base - 39хв відео

---
+++ src/myvueproject1/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <h1>{{message}}</h1>
+    <h1>{{age1}}</h1>
+    <h1 v-once>{{ message }}</h1>         // якщо треба перерендерити сторінку, щоб лише РАЗ вивести оригінальне
+                                          // повідомлення, а не переприсвоєне методом, то використовуємо v-once
+    <p>{{sayHello()}}</p>
+    <p>
+      <a v-bind:href="link">Google</a>
+    </p>
+    <span v-html="finishedLink"></span>
+  </div>
+
+
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data () {
+    return {
+      message: "Hello World1",
+      age1: 30,
+      link: "http://google.com",
+      finishedLink: "<a href='http://google.com'>Google</a>"
+    }
+  },
+  methods: {
+    sayHello() {
+      this.message = "Hello only"; // при потребі таким чином можна переприсвоїти значення ВСЮДИ де використовував message
+      return this.message       // в методах викликаємо функцію message тільки через this
+
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/myvueproject1/src/components/HelloWorld.vue
@@ -0,0 +1,27 @@
+<template>
+
+<div>
+    <h1>{{message}}</h1>
+    <h2></h2>
+
+
+</div>
+
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+
+    data() {
+      return {
+          message: "Hello World New"
+      }
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
