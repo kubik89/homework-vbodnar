787f873ddbefdc176863d4948f96bfb4dfd6443d
Volodymyr <kubik89@yahoo.com>
2020-08-24 12:31:40 +0300

    start dz

--- src/myvueproject1/src/App.vue
+++ src/myvueproject1/src/App.vue
@@ -1,36 +1,24 @@
 <template>
     <div>
-        <ul>
-            <li v-for="(item, i) in listTodo" :key="i">
-                <span>{{item}}</span>
-                <button v-on:click="remove(i)">Del</button>
-            </li>
-        </ul>
-        <input type="text" v-model="text">
-        <button v-on:click="add" v-if="listTodo.length<=5">Add</button>
+        <h1>{{msg}}</h1>
+<!--        при кліку на першу кнопку назва кнопки зміниться на Changed-->
+        <button @click="msg = 'Changed'">Update Title</button>
+        <button @click="destroy">Destroy</button>
     </div>
 </template>
 
 <script>
-
     export default {
         name: 'App',
         data() {
             return {
-                text: "",
-                listTodo: [
-                    "sleep", "eat"
-                ]
+                msg: "Hello Vue"
             }
         },
 
         methods: {
-            add() {
-                this.listTodo.push(this.text);
-                this.text = ""
-            },
-            remove(i) {
-                this.listTodo.splice(i, 1)
+            destroy() {
+
             }
         }
     }
---
+++ src/myvueproject1/src/dz-Less1.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+        <ul>
+            <li v-for="(item, i) in listTodo" :key="i">
+                <span>{{item}}</span>
+                <button v-on:click="remove(i)">Del</button>
+            </li>
+        </ul>
+        <input type="text" v-model="text">
+        <button v-on:click="add" v-if="listTodo.length<=5">Add</button>
+    </div>
+</template>
+
+<script>
+
+    export default {
+        name: 'App',
+        data() {
+            return {
+                text: "",
+                listTodo: [
+                    "sleep", "eat"
+                ]
+            }
+        },
+
+        methods: {
+            add() {
+                this.listTodo.push(this.text);
+                this.text = ""
+            },
+            remove(i) {
+                this.listTodo.splice(i, 1)
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
