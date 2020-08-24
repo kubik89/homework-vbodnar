0c2e4b7e38f2d0dc3fe472acdf582ae4f738e4f9
Volodymyr <kubik89@yahoo.com>
2020-08-24 12:56:11 +0300

    20 min of video Less2

--- src/myvueproject1/src/App.vue
+++ src/myvueproject1/src/App.vue
@@ -18,8 +18,43 @@
 
         methods: {
             destroy() {
-
+                this.$destroy()
             }
+        },
+            //Такий метод запускається під капотом, він дивиться, що є в data, які методи...
+            //Як видно вони винесені із методів
+            beforeCreate() {
+                console.log("before create")
+            },
+            //такий метод виконує компіляцію всіх змінних, проводить всі обчислення методів
+            created() {
+                console.log("created")
+            },
+            //цей метод замінює всі значення змінних в html компоненти, наприклад змінна msg передає своє значення в h1
+            beforeMount() {
+                console.log("before mount")
+            },
+            //тут шаблон прикріплюється до DOM дерева
+            mounted() {
+                console.log("mounted")
+            },
+        //тут проводиться повторний рендеринг сторінки
+        beforeUpdate() {
+            console.log("before update")
+        },
+        // оновлення того, що було потрібно для рендерингу. По суті при оновленні чогось два методи
+        // beforeUpdate і updated запускаються разом
+        updated() {
+            console.log("updated")
+        },
+        //запускається коли ми закриваємо сторінку чи додаток
+        //beforeDestroy і destroyed запускаються разом
+        beforeDestroy() {
+            console.log("beforeDestroy")
+        },
+        //після закриття додатку цей метод видаляє обєкт Vue із памяті і ми більше працювати з ним не можемо
+        destroyed() {
+            console.log("destroyed")
         }
     }
 </script>
